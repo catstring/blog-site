c150cd80ac7590dcb7d7fe59bdf49d1d1b0cc44e
Todd MC Tsai <toddmctsai@gmail.com>
2024-07-24 20:42:52 +0800

    isolate blogposts

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 // src/App.tsx
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BlogPosts from './routes/BlogPosts';
 import { useTheme } from './contexts/ThemeContext';
 
 const App: React.FC = () => {
-  const [searchQuery, setSearchQuery] = useState<string>('');
   const { theme } = useTheme(); // Use theme context
 
   return (
     <div className={`min-h-screen ${theme === 'dark' ? 'bg-stone-900 text-stone-100' : 'bg-white text-black'}`}>
       <Routes>
-        <Route path="/" element={<BlogPosts searchQuery={searchQuery} />} />
+        <Route path="/" element={<BlogPosts searchQuery="" />} />
       </Routes>
     </div>
   );
