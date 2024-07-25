341b81d831d81ef0bb20ffba1a679cbded54c1cd
Todd MC Tsai <toddmctsai@gmail.com>
2024-07-25 17:43:16 +0800

    added manual chunking

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,18 @@ export default defineConfig({
   },
   define: {
     'process.env': process.env
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Splitting each library into its own chunk
+            return id.toString().split('node_modules/')[1].split('/')[0];
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 600, // Increasing the chunk size warning limit
   }
 });
