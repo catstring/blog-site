71cc28761f6722d6765b0ca076d8aab2025420b1
Todd MC Tsai <toddmctsai@gmail.com>
2024-07-25 16:57:44 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// src/App.tsx
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import Admin from './routes/Login';
