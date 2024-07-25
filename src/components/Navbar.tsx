d8ae53b8481a9f56577f82eac00e4ec80f8df9fd
Todd MC Tsai <toddmctsai@gmail.com>
2024-07-25 17:19:21 +0800

    Revert to commit 7a7eca0

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,6 +21,7 @@ const NavBar: React.FC<NavBarProps> = ({
   theme,
   isLoggedIn,
   onLogout,
+  onLogin,
   toggleTheme,
   openSearch,
   formQuery,
@@ -40,6 +41,12 @@ const NavBar: React.FC<NavBarProps> = ({
           {isDropdownOpen && (
             <div className={`absolute left-0 mt-2 w-48 py-2 ${theme === 'dark' ? 'bg-stone-700' : 'bg-white'} rounded-md shadow-lg z-20`}>
               <div
+                onClick={isLoggedIn ? onLogout : onLogin}
+                className={`block px-4 py-2 text-sm cursor-pointer ${theme === 'dark' ? 'text-stone-100' : 'text-black'} hover:${theme === 'dark' ? 'bg-stone-600' : 'bg-gray-200'}`}
+              >
+                {isLoggedIn ? 'Logout' : 'Login'}
+              </div>
+              <div
                 onClick={toggleTheme}
                 className={`block px-4 py-2 text-sm cursor-pointer ${theme === 'dark' ? 'text-stone-100' : 'text-black'} hover:${theme === 'dark' ? 'bg-stone-600' : 'bg-gray-200'}`}
               >
@@ -55,8 +62,8 @@ const NavBar: React.FC<NavBarProps> = ({
         </Link>
       </div>
       <div className="flex-1 hidden sm:flex justify-center">
-        <form onSubmit={handleSearchSubmit} className="relative w-full max-w-md">
-          <input
+      <form onSubmit={handleSearchSubmit} className="relative w-full max-w-md">
+        <input
             type="text"
             value={formQuery}
             onChange={(e) => setFormQuery(e.target.value)}
@@ -64,37 +71,36 @@ const NavBar: React.FC<NavBarProps> = ({
             onBlur={() => setIsFocused(false)}
             placeholder="Search"
             className={`pl-5 p-2 border rounded-full w-full pr-10 ${
-              theme === 'dark'
+            theme === 'dark'
                 ? 'bg-stone-900 text-stone-500 border-stone-600 placeholder-stone-500 font-thin'
                 : 'bg-white text-gray-500 border-gray-300 placeholder-gray-500 font-thin'
             }`}
-          />
-          <div className="absolute right-0 top-0 h-full flex items-center pr-3">
+        />
+        <div className="absolute right-0 top-0 h-full flex items-center pr-3">
             <button type="submit" className="hidden">Search</button>
             {isFocused && (
-              <i
-                className={`fa-solid fa-circle-xmark cursor-pointer ${theme === 'dark' ? 'text-stone-500' : 'text-gray-500'}`}
+            <i
+                className={`fa-solid fa-circle-xmark cursor-pointer ${
+                theme === 'dark' ? 'text-stone-500' : 'text-gray-500'
+                }`}
                 onMouseDown={handleClearSearch}
-              ></i>
+            ></i>
             )}
-          </div>
+        </div>
         </form>
       </div>
       <div className="flex items-center space-x-4">
         <div className="sm:hidden">
           <i className={`fa-solid fa-magnifying-glass cursor-pointer ${theme === 'dark' ? 'text-stone-100' : 'text-black'}`} onClick={openSearch}></i>
         </div>
-        {isLoggedIn && (
           <>
             <Link to="/create" className={`flex items-center text-xl ${theme === 'dark' ? 'text-stone-100' : 'text-black'}`}>
               <i className="fa-regular fa-square-plus"></i>
             </Link>
             <Link to="/admin-blog-posts" className={`flex items-center ${theme === 'dark' ? 'text-stone-100' : 'text-black'}`}>
-              <i className="fa-solid fa-toolbox"></i>
+              <i className="fa-solid fa-user-tie"></i>
             </Link>
-            <i className={`fa-solid fa-right-from-bracket cursor-pointer ${theme === 'dark' ? 'text-stone-100' : 'text-black'}`} onClick={onLogout}></i>
           </>
-        )}
       </div>
     </nav>
   );
