1cfad13ecc32bc6371b2adc0e741987eb1c2b636
Todd MC Tsai <toddmctsai@gmail.com>
2024-09-11 19:30:41 +0800

    revert randomize tag filter

--- src/components/TagFilter.tsx
+++ src/components/TagFilter.tsx
@@ -1,3 +1,4 @@
+// src/components/TagFilter.tsx
 import React from 'react';
 
 interface Tag {
@@ -13,19 +14,7 @@ interface TagFilterProps {
   theme: string;
 }
 
-// Randomize Array (Fisher-Yates Shuffle Algorithm)
-const shuffleArray = (array: Tag[]) => {
-  const shuffledArray = [...array];
-  for (let i = shuffledArray.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
-  }
-  return shuffledArray;
-};
-
 const TagFilter: React.FC<TagFilterProps> = ({ tags, selectedTags, onTagClick, onAllClick, theme }) => {
-  const shuffledTags = shuffleArray(tags);
-
   return (
     <div className="px-4 flex flex-nowrap overflow-x-auto gap-2 mb-4 scrollbar-hide">
       <span
@@ -34,7 +23,7 @@ const TagFilter: React.FC<TagFilterProps> = ({ tags, selectedTags, onTagClick, o
       >
         All
       </span>
-      {shuffledTags.map(tag => (
+      {tags.map(tag => (
         <span
           key={tag.id}
           onClick={() => onTagClick(tag.name)}
