26bdeba547eeabe05b017480c678cb0e3ad495de
Todd MC Tsai <toddmctsai@gmail.com>
2024-09-11 18:31:22 +0800

    randomize tag filter, hide scroll bar

--- src/components/TagFilter.tsx
+++ src/components/TagFilter.tsx
@@ -1,4 +1,3 @@
-// src/components/TagFilter.tsx
 import React from 'react';
 
 interface Tag {
@@ -14,16 +13,28 @@ interface TagFilterProps {
   theme: string;
 }
 
+// Randomize Array (Fisher-Yates Shuffle Algorithm)
+const shuffleArray = (array: Tag[]) => {
+  const shuffledArray = [...array];
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+  return shuffledArray;
+};
+
 const TagFilter: React.FC<TagFilterProps> = ({ tags, selectedTags, onTagClick, onAllClick, theme }) => {
+  const shuffledTags = shuffleArray(tags);
+
   return (
-    <div className="px-4 flex flex-nowrap overflow-x-auto gap-2 mb-4">
+    <div className="px-4 flex flex-nowrap overflow-x-auto gap-2 mb-4 scrollbar-hide">
       <span
         onClick={onAllClick}
         className={`px-4 py-2 text-sm font-medium cursor-pointer rounded-full ${selectedTags.length === 0 ? theme === 'dark' ? 'bg-white text-black' : 'bg-black text-white' : theme === 'dark' ? 'bg-stone-700 text-white' : 'bg-stone-200 text-black'}`}
       >
         All
       </span>
-      {tags.map(tag => (
+      {shuffledTags.map(tag => (
         <span
           key={tag.id}
           onClick={() => onTagClick(tag.name)}
